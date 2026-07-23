901dfa8d8bd51dcc788113d843c819c4666343b7
Ertoba <49425381+Ertoba@users.noreply.github.com>
2026-07-23 09:25:47 +0400

    Keep Keepz route registration minimal

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,10 +10,27 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to the "home" route for your application.
+     *
+     * This is used by Laravel authentication to redirect users after login.
+     *
+     * @var string
+     */
     public const HOME = '/home';
 
-    protected $namespace = 'App\Http\Controllers';
+    /**
+     * The controller namespace for the application.
+     *
+     * When present, controller route declarations will automatically be prefixed with this namespace.
+     *
+     * @var string|null
+     */
+    protected $namespace = 'App\\Http\\Controllers';
 
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     */
     public function boot(): void
     {
         $this->configureRateLimiting();
@@ -33,6 +50,9 @@ class RouteServiceProvider extends ServiceProvider
         });
     }
 
+    /**
+     * Configure the rate limiters for the application.
+     */
     protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
